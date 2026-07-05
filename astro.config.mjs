bfbb3ad53e5d9db4e95882cf1cc46e4b89c16c8a
Marco Picanço <marcopicanco.dev@gmail.com>
2026-07-04 23:58:19 -0300

    criação de uma nova index

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,6 @@ import expressiveCode from 'astro-expressive-code'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.marcopicanco.com',
-  redirects: {
-    '/': '/portfolio/#inicio',
-  },
   integrations: [
     tailwind(),
     expressiveCode({
